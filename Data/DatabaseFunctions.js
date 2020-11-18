fed2dcb1b6ccd6b7c10544262cd7a3fc72be8bb7
JoeReynolds07 <joecreynolds07@gmail.com>
2020-11-17 22:14:02 -0700

    Begin adding data to DB

--- Data/DatabaseFunctions.js
+++ Data/DatabaseFunctions.js
@@ -2,7 +2,62 @@ require("dotenv/config");
 const MongoClient = require("mongodb").MongoClient;
 const mongoClient = new MongoClient(process.env.DB_CONNECTION, { useUnifiedTopology: true });
 const logger = require("../Logs/Logger");
+// Collection constants
+const dbCollections = {
+    GEAR_SETUPS: "GearSetups",
+    BOSS_INFO: "BossInfo",
+    RSN_LIST: "RSNList",
+    EQUIPMENT: "Equipment"
+}
 
+const testSetup1 = require("../Setups/dks/1.json");
+const testSetup2 = require("../Setups/dks/2.json");
+
+async function testFunc() {
+    await addToGearSetups("dks", testSetup1);
+    await addToGearSetups("dks", testSetup2);
+    await closeConnection();
+}
+
+/**
+ * Adds a setup to the given boss (key) in GearSetups collection.
+ * @param bossName The name of the boss is the key in the GearSetups collection
+ * @param setupJSON The JSON setup to add
+ * @returns {Promise<void>}
+ */
+async function addToGearSetups(bossName, setupJSON) {
+    const database = await connectToDB();
+    const collection = database.collection(dbCollections.GEAR_SETUPS);
+    try {
+        const modifiedJSON = {
+            _id: "dks",
+            setups: [setupJSON]
+        }
+        // Add the setup to the DB in GearSetups collection
+        await collection.insertOne(modifiedJSON);
+        logger.logDB("Added setup");
+    } catch (err) {
+        logger.logDB(`**ERROR ADDING ${setupJSON.name} TO GEARSETUPS COLLECTION:** ` + err);
+    }
+}
+
+/**
+ * Helper function for closing connection.
+ * @returns {Promise<void>}
+ */
+async function closeConnection() {
+    try {
+        await mongoClient.close();
+        logger.logDB("Success closing connection");
+    } catch (err) {
+        logger.logDB("**ERROR CLOSING CONNECTION**: " + err);
+    }
+}
+
+/**
+ * Helper function for connecting to the DB and returns the database object.
+ * @returns {Promise<Db>}
+ */
 async function connectToDB() {
     try {
         // Create the connection using credentials in .env and log events using logger
@@ -10,25 +65,15 @@ async function connectToDB() {
         logger.logDB("Connected to database.")
 
         // If the DB Boss Gear Bot does not exist, it will create it
-        const database = mongoClient.db("BossGearBotDB");
-        // This creates a new collection inside the DB that was created (or exists already)
-        const collection = database.collection("GearSetups");
-        await collection.insertOne({
-            name: "test",
-            age: "30 yikes"
-        });
-        try {
-            // This clears the collection
-            await database.collection("GearSetups").drop();
-            logger.logDB("**DROPPED COLLECTION GearSetups**");
-        } catch (err) {
-            logger.logDB("**DATABASE ERROR**: " + err);
-        }
-        // Close the connection once we are done with it.
-        await mongoClient.close();
+        return mongoClient.db("BossGearBotDB");
+        //const collection = database.collection("GearSetups");
     } catch (err) {
         logger.logDB("**DATABASE ERROR**: " + err)
     }
 }
 
-connectToDB();
+testFunc();
+
+module.exports = {
+    dbCollections
+}
